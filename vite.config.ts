05cd9ebc18477f50be1b8bde08fdaf1f460be6ac
vajjipartisatwikraj <satwikrajv@gmail.com>
2026-05-22 07:32:26 +0530

    Modified UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     tanstackStart(),
     react(),
   ],
